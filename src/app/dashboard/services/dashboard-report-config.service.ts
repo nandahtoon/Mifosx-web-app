0e7911f283f7f417c6e97fc645b4375a606b3e78
Nanda Htoon <167466481+nandahtoon@users.noreply.github.com>
2026-06-27 16:50:04 -0400

    feat: reconcile dashboard report config service

---
+++ src/app/dashboard/services/dashboard-report-config.service.ts
@@ -0,0 +1,23 @@
+/**
+ * Copyright since 2025 Mifos Initiative
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+/** Angular Imports */
+import { Injectable } from '@angular/core';
+
+/** Custom Models */
+import { DashboardReportConfig } from '../models/dashboard-api.model';
+
+@Injectable({ providedIn: 'root' })
+export class DashboardReportConfigService {
+  getTotalPortfolioReportConfig(): DashboardReportConfig {
+    return {
+      reportName: null,
+      params: {}
+    };
+  }
+}
