e7c1d8a01784f91763613c5af3398abce82095a9
Nanda Htoon <167466481+nandahtoon@users.noreply.github.com>
2026-06-27 16:50:51 -0400

    feat: reconcile dashboard state component

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -47,5 +47,9 @@ import { DashboardFacadeService } from './services/dashboard-facade.service';
 export class DashboardComponent {
   private readonly dashboardFacade = inject(DashboardFacadeService);
 
-  readonly dashboard$ = this.dashboardFacade.getDashboardViewModel();
+  dashboardState$ = this.dashboardFacade.getDashboardState();
+
+  reloadDashboard(): void {
+    this.dashboardState$ = this.dashboardFacade.getDashboardState();
+  }
 }
