910aef632482a302ca143a4a986f11a188e5dd36
Nanda Htoon <167466481+nandahtoon@users.noreply.github.com>
2026-06-27 13:58:39 -0400

    feat: add dashboard PAR summary component

---
+++ src/app/dashboard/components/dashboard-par-summary/dashboard-par-summary.component.ts
@@ -0,0 +1,17 @@
+/**
+ * Copyright since 2025 Mifos Initiative
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'mifosx-dashboard-par-summary',
+  templateUrl: './dashboard-par-summary.component.html',
+  styleUrls: ['./dashboard-par-summary.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class DashboardParSummaryComponent {}
