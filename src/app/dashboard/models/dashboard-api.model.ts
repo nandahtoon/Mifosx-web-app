f095fb6c995c617d4c4ab5fed86958062f52803f
Nanda Htoon <167466481+nandahtoon@users.noreply.github.com>
2026-06-27 14:43:08 -0400

    feat: add dashboard api models

---
+++ src/app/dashboard/models/dashboard-api.model.ts
@@ -0,0 +1,20 @@
+/**
+ * Copyright since 2025 Mifos Initiative
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+export interface DashboardRunReportParams {
+  [key: string]: string | number | boolean;
+}
+
+export interface DashboardRunReportResponse {
+  data?: Array<{ row?: string[] | Record<string, unknown> }>;
+  columnHeaders?: Array<{ columnName?: string; columnType?: string }>;
+}
+
+export interface DashboardApiError {
+  message: string;
+}
