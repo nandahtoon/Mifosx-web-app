d908cc6c1c9d38d86f89056e88f1ed42f7adc19d
Nanda Htoon <167466481+nandahtoon@users.noreply.github.com>
2026-06-27 14:02:28 -0400

    refactor: accept activity rows as input

--- src/app/dashboard/components/dashboard-activity-summary/dashboard-activity-summary.component.ts
+++ src/app/dashboard/components/dashboard-activity-summary/dashboard-activity-summary.component.ts
@@ -6,12 +6,17 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { DashboardMetricRow } from '../../models/dashboard.model';
 
 @Component({
   selector: 'mifosx-dashboard-activity-summary',
   templateUrl: './dashboard-activity-summary.component.html',
   styleUrls: ['./dashboard-activity-summary.component.scss'],
+  imports: [NgFor],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class DashboardActivitySummaryComponent {}
+export class DashboardActivitySummaryComponent {
+  @Input({ required: true }) activities: DashboardMetricRow[] = [];
+}
