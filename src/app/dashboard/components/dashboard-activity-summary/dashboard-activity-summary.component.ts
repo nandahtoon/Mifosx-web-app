f72df7c5e09b7659f5c8b21a21f942d0e5bf13d3
Nanda Htoon <167466481+nandahtoon@users.noreply.github.com>
2026-06-27 13:58:10 -0400

    feat: add dashboard activity summary component

---
+++ src/app/dashboard/components/dashboard-activity-summary/dashboard-activity-summary.component.ts
@@ -0,0 +1,17 @@
+/**
+ * Copyright since 2025 Mifos Initiative
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'mifosx-dashboard-activity-summary',
+  templateUrl: './dashboard-activity-summary.component.html',
+  styleUrls: ['./dashboard-activity-summary.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class DashboardActivitySummaryComponent {}
